49d0afa7f15f67d3b5cf6331e29edbb5b3b60128
Adalto Selau Sparremberger <adaltoss@me.com>
2021-05-28 22:31:01 -0300

    Aula14 - 28/05/2021 - AppProdutos construído para consumir API externa - implementado o cadastro de produto.

--- AppProdutos/app/src/main/java/br/pro/adalto/appprodutos/MainActivity.java
+++ AppProdutos/app/src/main/java/br/pro/adalto/appprodutos/MainActivity.java
@@ -1,13 +1,21 @@
 package br.pro.adalto.appprodutos;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -36,12 +44,110 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        menu.add("Novo Produto");
+        menu.add( getResources().getString(R.string.txtFechar) );
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if( item.toString().equals( getResources().getString(R.string.txtFechar) ) ){
+            finish();
+        }
+        if( item.toString().equals( "Novo Produto"  ) ){
+            addProduto();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void addProduto(){
+
+        AlertDialog.Builder alerta = new AlertDialog.Builder(this);
+        alerta.setTitle("Adicionar novo Produto...");
+        alerta.setIcon( android.R.drawable.ic_menu_add  );
+        EditText etNome = new EditText(this);
+        etNome.setHint("Digite aqui o nome do produto...");
+        alerta.setView( etNome );
+        alerta.setNeutralButton("Cancelar", null);
+        alerta.setPositiveButton("Salvar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String nome = etNome.getText().toString();
+                if( nome.isEmpty() ){
+                    Toast.makeText(MainActivity.this,
+                            "O nome deve ser preenchido...", Toast.LENGTH_LONG).show();
+                }else {
+                    AddProduto addProduto = new AddProduto();
+                    addProduto.execute("nome="+ nome  + "&preco=0.0&quantidade=0.0" );
+                }
+            }
+        });
+        alerta.show();
+    }
+
+    private class AddProduto extends AsyncTask<String, String, String>{
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressBar.setActivated( true );
+            progressBar.setVisibility( View.VISIBLE );
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            return Servidor.executar("addProdutos.php",  strings );
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Log.i("servidor" , s );
+            if( s != null ){
+                try {
+                    JSONObject json = new JSONObject( s );
+                    int id =  json.getInt("id");
+                    if( id > 0 ){
+                        atualizar();
+                        Toast.makeText(MainActivity.this,
+                                "Produto inserido, id: "+ id , Toast.LENGTH_LONG).show();
+                    }else {
+                        Toast.makeText(MainActivity.this,
+                                "Erro ao inserir o Produto!" , Toast.LENGTH_LONG).show();
+                    }
+                }catch (JSONException e){
+                    Toast.makeText(MainActivity.this,
+                            "Erro no servidor 1!" , Toast.LENGTH_LONG).show();
+                }
+            }else{
+                Toast.makeText(MainActivity.this,
+                        "Erro no servidor 2!" , Toast.LENGTH_LONG).show();
+            }
+            progressBar.setActivated(false);
+            progressBar.setVisibility( View.INVISIBLE );
+        }
+    }
+
+
+
+
+
+    @Override
     protected void onResume() {
         super.onResume();
 
+        atualizar();
+    }
+
+    private void atualizar(){
         BuscaProduto conexao =new BuscaProduto();
         conexao.execute();
     }
+
+
 
     private class BuscaProduto extends AsyncTask<String, String, String>{
 
